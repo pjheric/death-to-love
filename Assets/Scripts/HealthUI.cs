631ec58d484bceb27e73166e5fe5b7851d6bfba3
pjheric <pjheric1219@gmail.com>
2022-04-21 15:12:44 -0700

    Set up segmented health bar

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -6,35 +6,30 @@ public class HealthUI : MonoBehaviour
 {
     [SerializeField] private Image healthBar;
     [SerializeField] private Image[] healthTicks;
-    [SerializeField] private float health, maxHealth = 100; //Delete later, here for testing only
+    [SerializeField] private float maxHealth = 30; //Delete later, here for testing only
+    [SerializeField] private FloatAsset health; 
     [SerializeField] private float lerpSpeed = 3f;
 
     private void Start()
     {
-        health = maxHealth; 
+        health.Value = maxHealth; 
     }
     private void Update()
     {
-        if(health > maxHealth)
+        if(health.Value > maxHealth)
         {
-            health = maxHealth; //Prevents overhealing 
+            health.Value = maxHealth; //Prevents overhealing 
         }
         lerpSpeed *= Time.deltaTime;
         FillBar();
-        ChangeColor(); 
     }
     private void FillBar()
     {
-        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health / maxHealth), lerpSpeed);
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health.Value / maxHealth), lerpSpeed);
         for(int i = 0; i < healthTicks.Length; i++)
         {
-            healthTicks[i].enabled = !DisplayHealthPoint(health, i); 
+            healthTicks[i].enabled = !DisplayHealthPoint(health.Value, i); 
         }
-    }
-    private void ChangeColor()
-    {
-        Color healthColor = Color.Lerp(Color.red, Color.green, (health / maxHealth));
-        healthBar.color = healthColor; 
     }
     bool DisplayHealthPoint(float _health, int pointNumber)
     {
