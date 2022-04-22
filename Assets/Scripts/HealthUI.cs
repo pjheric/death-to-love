f53a7910dbfcbaf279d1bdb752cf7dfce2f1dfc2
Nathan More <morenate2000@gmail.com>
2022-04-22 13:49:37 -0700

    Fixed updated health bar

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -1,3 +1,4 @@
+// Contributor(s): Eric Park, Nathan More
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,17 @@ public class HealthUI : MonoBehaviour
 {
     [SerializeField] private Image healthBar;
     [SerializeField] private Image[] healthTicks;
-    [SerializeField] private float maxHealth = 30; //Delete later, here for testing only
-    [SerializeField] private FloatAsset health; 
+    [SerializeField] private float maxHealth = 30f; //Delete later, here for testing only
+    [SerializeField] private FloatAsset health;
     [SerializeField] private float lerpSpeed = 3f;
+
+    private float healthBarStep; // Determines how many health points each tick represents
 
     private void Start()
     {
-        health.Value = maxHealth; 
+        health.Value = maxHealth;
+
+        healthBarStep = maxHealth / healthTicks.Length; // Sets healthBarStep based on max health and num of ticks
     }
     private void Update()
     {
@@ -26,13 +31,14 @@ public class HealthUI : MonoBehaviour
     private void FillBar()
     {
         healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (health.Value / maxHealth), lerpSpeed);
+
         for(int i = 0; i < healthTicks.Length; i++)
         {
             healthTicks[i].enabled = !DisplayHealthPoint(health.Value, i); 
         }
     }
-    bool DisplayHealthPoint(float _health, int pointNumber)
+    bool DisplayHealthPoint(float health, int pointNumber)
     {
-        return ((pointNumber * healthTicks.Length) >= _health);
+        return ((pointNumber * healthBarStep) >= health);
     }
 }
