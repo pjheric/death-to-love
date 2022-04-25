3d4dca27e9ff93350096851fef835c3672972df3
Nathan More <morenate2000@gmail.com>
2022-04-25 15:00:18 -0700

    Got Character-Select Screen Working

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,3 +1,4 @@
+// Contributor(s): Nathan More
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Players/CharacterSelectIcon.cs
+++ Assets/Scripts/Players/CharacterSelectIcon.cs
@@ -1,3 +1,4 @@
+// Contributor(s): Nathan More
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,43 +8,86 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(SpriteRenderer))]
 public class CharacterSelectIcon : MonoBehaviour
 {
+
+    public enum CharacterChoices { Liz, Jay, None};
+    public CharacterChoices _selectedChar;
+
     [SerializeField] private Sprite _keyboardSprite;
     [SerializeField] private Sprite _controllerSprite;
+    [SerializeField] private float _characterSelectOffsetX;
+
+    private Vector3 _initialPos;
+    private Vector3 _leftCharacterPos;
+    private Vector3 _rightCharacterPos;
 
     private PlayerInput _playerInput;
     private SpriteRenderer _spriteRenderer;
-    //private List<Transform> _
-
-    public void OnSelect(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            if (_playerInput.currentControlScheme == "Keyboard")
-            {
-                _spriteRenderer.sprite = _keyboardSprite;
-            }
-            else if (_playerInput.currentControlScheme == "Controller")
-            {
-                _spriteRenderer.sprite = _controllerSprite;
-            }
-        }
-    }
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        SetSprite();
+        _selectedChar = CharacterChoices.None;
     }
 
     private void Start()
     {
-        //if (_playerInput.currentControlScheme == "Keyboard")
-        //{
-        //    _spriteRenderer.sprite = _keyboardSprite;
-        //}
-        //else if (_playerInput.currentControlScheme == "Controller")
-        //{
-        //    _spriteRenderer.sprite = _controllerSprite;
-        //}
+        _initialPos = gameObject.transform.position;
+        _leftCharacterPos = _initialPos - new Vector3(_characterSelectOffsetX, 0, 0);
+        _rightCharacterPos = _initialPos + new Vector3(_characterSelectOffsetX, 0, 0);
+    }
+
+    public void SetSprite()
+    {
+        if (_playerInput.currentControlScheme == "Keyboard")
+        {
+            _spriteRenderer.sprite = _keyboardSprite;
+        }
+        else if (_playerInput.currentControlScheme == "Controller")
+        {
+            _spriteRenderer.sprite = _controllerSprite;
+        }
+    }
+
+    public void OnSelect(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //SetSprite();
+
+            MoveSelector(context.ReadValue<Vector2>().x);
+        }
+    }
+
+    public void MoveSelector(float xDirection)
+    {
+        if (xDirection > 0.5)
+        {
+            if (gameObject.transform.position.x == _initialPos.x)
+            {
+                gameObject.transform.position = _rightCharacterPos;
+                _selectedChar = CharacterChoices.Jay;
+            }
+            else if (gameObject.transform.position.x == _leftCharacterPos.x)
+            {
+                gameObject.transform.position = _initialPos;
+                _selectedChar = CharacterChoices.None;
+            }
+        }
+        else if (xDirection < -0.5)
+        {
+            if (gameObject.transform.position.x == _initialPos.x)
+            {
+                gameObject.transform.position = _leftCharacterPos;
+                _selectedChar = CharacterChoices.Liz;
+            }
+            else if (gameObject.transform.position.x == _rightCharacterPos.x)
+            {
+                gameObject.transform.position = _initialPos;
+                _selectedChar = CharacterChoices.None;
+            }
+        }
     }
 }
