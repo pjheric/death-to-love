cfef5bde172c7767dfabfd6cad4536eb2f538be5
pjheric <pjheric1219@gmail.com>
2022-05-02 15:30:56 -0700

    Started polish on character select, finished start menu for now

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement; 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] GameObject _mainMenuPanel;
+
     private LevelLoader levelLoader;
     private void Start()
     {
         levelLoader = GameObject.FindGameObjectWithTag("LevelLoader").GetComponent<LevelLoader>();
+    }
+    public void OpenMenu(GameObject _panel)
+    {
+        _mainMenuPanel.SetActive(false);
+        _panel.SetActive(true); 
+    }
+    public void CloseMenu(GameObject _panel)
+    {
+        _mainMenuPanel.SetActive(true);
+        _panel.SetActive(false); 
     }
     public void StartGame()
     {
