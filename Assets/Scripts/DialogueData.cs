64f8455628f29d1cf1cea1086f92ef31a652f544
pjheric <pjheric1219@gmail.com>
2022-05-08 18:11:22 -0700

    Dialogue data, heat UI prototype

---
+++ Assets/Scripts/DialogueData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue Data", menuName = "ScriptableObjects/DialogueData", order = 2)]
+public class DialogueData : ScriptableObject
+{
+    [System.Serializable]
+    private struct DialoguePair
+    {
+        public string speaker;
+        public string line; 
+    }
+    [SerializeField] List<DialoguePair> dialogue; 
+    //Dialogue itself is stored as a string value; the speaker's name is stored as a string key 
+    public string GetLine(int lineNum)
+    {
+        return dialogue[lineNum].line; 
+    }
+    public string GetSpeaker(int lineNum)
+    {
+        return dialogue[lineNum].speaker;
+    }
+
+}
