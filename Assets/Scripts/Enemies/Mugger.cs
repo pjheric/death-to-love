df9565df28e275dad0a1614f9fcadf001271c21e
Qrow99 <bkeyro99@gmail.com>
2022-05-20 16:03:01 -0700

    Update Mugger.cs

--- Assets/Scripts/Enemies/Mugger.cs
+++ Assets/Scripts/Enemies/Mugger.cs
@@ -64,7 +64,7 @@ public class Mugger : EnemyAgent
                     {
                         attack();
                     }
-                    else)
+                    else
                     {
 
                         backup();
